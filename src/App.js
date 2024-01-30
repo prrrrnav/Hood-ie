4dd2305417f3a68ca49dac967ee1569c591f6ad9
Pranav <Pranav143010@gmail.com>
2024-01-31 02:35:05 +0530

    Added search but product filteration pending

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Navbar from './Navbar';
-
 import Products from './Products';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+  const [searchData, setSearchData] = useState(null)
+
+  const fixingChildDataNeed = (data) => {
+    setSearchData(data)
+    console.log("data searhched",searchData);
+  }
+  
 
 
   return (
     <div className="App">
-      <Navbar />
-      <Products/>
+      <Navbar setSearchData={fixingChildDataNeed} />
+      <Products searchedItem={searchData}/>
     </div>
   );
 }
--- src/Products.js
+++ src/Products.js
@@ -1,50 +1,83 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import Product from './Product'
+import React, { useEffect, useState } from 'react';
+import Product from './Product';
 
-const Products = () => {
+const Products = (props) => {
+  const [productItinerary, setProductItinerary] = useState([]);
 
-    const [productItinerary, setProductItinerary] = useState([]);
-    let [titleItinerary,setTitleItinerary] = useState([])
+  // State to store product titles extracted from the product data
+  const [titleItinerary, setTitleItinerary] = useState([]);
+  const [lowerCaseTitles, setLowerCaseTitles] = useState([]);
 
-    async function fetchProducts() {
-      try {
-        let url = 'https://fakestoreapi.com/products';
-        let response = await fetch(url);
-        let data = await response.json();
-        setProductItinerary(data);
-        console.log(data)
-        function fetchTitles(){
-            data.map((key,index)=>{
-              titleItinerary.push(key.title)
-            })
-            console.log(titleItinerary)
-        }
-        fetchTitles()
-      } catch (error) {
-        console.error(error);
+
+
+  async function fetchProducts() {
+    try {
+      let url = 'https://fakestoreapi.com/products';
+      let response = await fetch(url);
+      let data = await response.json();
+
+      // Update the productItinerary state with the fetched data
+      setProductItinerary(data);
+      // console.log("printing library", data)
+
+      // Function to extract titles from the fetched data and update titleItinerary state
+      function fetchTitles() {
+        data.map((key, index) => {
+          titleItinerary.push(key.title);
+        });
+        
+      }
+      // Call the function to fetch titles
+      fetchTitles();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  useEffect(() => {
+    function compareStrings() {
+    
+      const lowerCaseTitlesCopy = []
+      titleItinerary.map((key, index) => {
+        let element = key.toLowerCase()
+        lowerCaseTitlesCopy.push(element)
+      })
+      setLowerCaseTitles(lowerCaseTitlesCopy)
+
+
+      const matchedString = lowerCaseTitles.find((title) => {
+        return title === props.searchedItem
+      })
+
+      if (matchedString) {
+        console.log("String are equal", matchedString)
       }
     }
-  
-    useEffect(() => {
-      fetchProducts();
-    }, []);
+    compareStrings()
+
+  }, [props.searchedItem])
 
 
+
+  // useEffect hook to fetch products when the component mounts
+  useEffect(() => {
+    fetchProducts();
+    // console.log(titleItinerary)
+  }, []);
 
   return (
     <div>
+      {/* Section header */}
       <h2 className='trending'>Trending</h2><br></br>
       <div className='products-box'>
-      
-      {productItinerary.map((value, index) => (
-        <Product key={index} image={value.image} title={value.title} desc={value.description} category={value.category} price={value.price} />
-      ))}
-
+        {/* Map through the productItinerary state to render Product components */}
+        {productItinerary.map((value, index) => (
+          <Product key={index} image={value.image} title={value.title} desc={value.description} category={value.category} price={value.price} />
+        ))}
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
 
-export default Products
+export default Products;
